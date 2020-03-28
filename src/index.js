154e87f60b1159a6f8cf3008e2c17afd0bf18fce
aditianshu <55697933+aditianshu@users.noreply.github.com>
2020-03-29 02:22:59 +0530

    changes to implement multiple dark theme

--- src/index.js
+++ src/index.js
@@ -1,40 +1,61 @@
 import React, { Component } from "react";
 import "./App.css";
 
-class Darktheme extends Component {
+class Darktheme extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = { value: 'select'};  }
+  
   componentDidMount() {
-    const presentTheme = localStorage.getItem("theme");
-    if (presentTheme) {
-      document.documentElement.setAttribute("data-theme", presentTheme);
-      if (presentTheme === "dark") {
-        document.querySelector('.switch input[type="checkbox"]').checked = true;
+      const presentTheme = localStorage.getItem("theme");
+      if (presentTheme) {
+        document.documentElement.setAttribute("data-theme", presentTheme);
+        }
+      else{
+        document.documentElement.setAttribute("data-theme", "normal");
+      }
+      }
+      
+    switchTheme(e) {
+      if (e.target.value === "darkGrey") {
+        document.documentElement.setAttribute("data-theme", "darkGrey");
+        localStorage.setItem("theme", "darkGrey");
+      } 
+      else if (e.target.value === "darkBlue") {
+        document.documentElement.setAttribute("data-theme", "darkBlue");
+        localStorage.setItem("theme", "darkBlue");
+      }
+       else if (e.target.value === "dark") {
+        document.documentElement.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
+       }
+       else {
+        document.documentElement.setAttribute("data-theme", "normal");
+        localStorage.setItem("theme", "normal");
       }
     }
-  }
-
-  switchTheme(e) {
-    if (e.target.checked) {
-      document.documentElement.setAttribute("data-theme", "dark");
-      localStorage.setItem("theme", "dark");
-    } else {
-      document.documentElement.setAttribute("data-theme", "normal");
-      localStorage.setItem("theme", "normal");
+  
+    onChange(e) {
+      this.setState({
+        value: e.target.value
+      })
+      this.switchTheme(e);
+    }
+  
+        
+   render() {
+            return (
+              <div className="container">
+              <label htmlFor="select1" ></label>
+              <select value={this.state.value} onChange={e => this.onChange(e)} className="form-control">
+                   <option value="select">Change Theme</option>
+                    <option value="normal">Normal</option>
+                    <option value="dark">Dark</option>
+                  <option value="darkGrey">Dark Grey</option>
+                    <option value="darkBlue">Dark Blue</option>
+              </select>
+              </div>
+      );
     }
   }
-
-  onChange(e) {
-    this.switchTheme(e);
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <label className="switch">
-          <input type="checkbox" onChange={e => this.onChange(e)} />
-          <span className="slider round"></span>
-        </label>
-      </div>
-    );
-  }
-}
 export default Darktheme;
